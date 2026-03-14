6cad654b27f08704940ac8e47f56182103f63d25
shiang723 <hanhanshiang6@gmail.com>
2026-03-14 10:06:15 -0700

    started creating CLI using argparse on main.py

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,16 @@
 import argparse
+parser = argparse.ArgumentParser(prog="gitscribe")
+parser.add_argument("type", help="what type of commit, either: modify, add, or remove", type= str, action="store_true")
+parser.add_argument("changed_filename", help="filename of changed file", type=str, action="store_true")
+parser.add_argument("old_filename", help="filename old file", type=str, action="store_true")
 
+args = parser.parse_args()
+commit_type = args.type
+
+if (commit_type == "modify"):
+    print("your changed file is " + args.changed_filename + "\nYour old file is: " + args.old_filename)
+   # getchange(args.filename)
+elif (commit_type == "add"):
+    print("added file: " + args.changed_filename)
+elif (commit_type == "remove"):
+    print("removed file: " + args.changed_filename)

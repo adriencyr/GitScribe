0e52cc1ef1c2fc6ce5dab7fc65149be56ababf19
shiang723 <hanhanshiang6@gmail.com>
2026-03-14 11:50:01 -0700

    added number of messages argument to gitscribe CLI on main.py

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,23 @@
 import argparse
 parser = argparse.ArgumentParser(prog="gitscribe")
-parser.add_argument("--type", type= str, choices=['modify', 'add', 'remove'], help="what type of commit, either: modify, add, or remove")
-parser.add_argument("changed_filename", help="filename of changed file", type=str)
-parser.add_argument("old_filename", help="filename old file", type=str, nargs='?', default='no_file_given')
+parser.add_argument("-t","--type", type= str, choices=['modify', 'add', 'remove'], help="what type of commit, either: modify, add, or remove")
+parser.add_argument( "-cf","--changed_filename", help="filename of changed file", type=str)
+parser.add_argument("-of","--old_filename", help="filename old file", type=str, nargs='?', default='no_file_given')
+parser.add_argument( "-nm",'--num_msgs', help="number of commit messages to generate", type=int, nargs='?', default=1)
 
 args = parser.parse_args()
 commit_type = args.type
 
 if (commit_type == "modify" and args.old_filename != "no_file_given"):
-    print("Your changed file is " + args.changed_filename + "\nYour old file is: " + args.old_filename)
+    for i in range(args.num_msgs):
+        print("Your changed file is " + args.changed_filename + "\nYour old file is: " + args.old_filename)
    # getchange(args.filename)
 elif (commit_type == "add"):
-    print("added file: " + args.changed_filename)
+    for i in range(args.num_msgs):
+        print("added file: " + args.changed_filename)
 elif (commit_type == "remove"):
-    print("removed file: " + args.changed_filename)
+    for i in range(args.num_msgs):
+        print("removed file: " + args.changed_filename)
 
 else:
     print("Input given not valid.")

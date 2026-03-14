89b2bc1022e8acc04dd33724f0bb0bffe59ba552
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-14 02:02:34 -0700

    feat: add user profile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import './App.css'
-import LoginButton from './components/LoginButton'
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
+  const { isAuthenticated, isLoading, error } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>Something went wrong: {error.message}</div>
+    );
+  }
 
   return (
-    <>
-     <LoginButton />
-    </>
-  )
+    <div>
+      {isAuthenticated ? (
+        <div>
+          <h2>Github user profile</h2>
+          <Profile />
+          <LogoutButton />
+        </div>
+      ) : (
+        <div>
+          <p>Sign in with Github</p>
+          <LoginButton />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;

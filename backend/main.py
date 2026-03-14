aee7895b72cf03668f3e87e4fc5484f9fc8a03b7
shiang723 <hanhanshiang6@gmail.com>
2026-03-14 10:54:36 -0700

    added dependency to gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 import argparse
 parser = argparse.ArgumentParser(prog="gitscribe")
-parser.add_argument("type", help="what type of commit, either: modify, add, or remove", type= str, action="store_true")
-parser.add_argument("changed_filename", help="filename of changed file", type=str, action="store_true")
-parser.add_argument("old_filename", help="filename old file", type=str, action="store_true")
+parser.add_argument("type", help="what type of commit, either: modify, add, or remove", type= str)
+parser.add_argument("changed_filename", help="filename of changed file", type=str)
+parser.add_argument("old_filename", help="filename old file", type=str)
 
 args = parser.parse_args()
 commit_type = args.type
@@ -14,3 +14,6 @@ elif (commit_type == "add"):
     print("added file: " + args.changed_filename)
 elif (commit_type == "remove"):
     print("removed file: " + args.changed_filename)
+
+else:
+    print("Type given not valid.")

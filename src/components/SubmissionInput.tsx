37131763e2dd9000afb641fc58a18d21ffc2fbf0
CelestiaWana <wyang73@my.bcit.ca>
2026-03-14 15:04:25 -0700

    button update (#9)
    * button update
    
    * update buttons
    
    * Upload Submission Button and upload File button

---
+++ src/components/SubmissionInput.tsx
@@ -0,0 +1,17 @@
+import { Button, HStack } from "@chakra-ui/react";
+import { RiArrowRightLine, RiMailLine } from "react-icons/ri";
+
+const SubmissionInput = () => {
+  return (
+    <HStack>
+      <Button colorPalette="teal" variant="solid">
+        <RiMailLine /> Upload File
+      </Button>
+      <Button colorPalette="teal" variant="outline">
+        Copy Text <RiArrowRightLine />
+      </Button>
+    </HStack>
+  );
+};
+
+export default SubmissionInput;

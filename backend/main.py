ae564c306d740664ad4df5842ee459738ad544a6
shiang723 <123040434+shiang723@users.noreply.github.com>
2026-03-14 11:31:23 -0700

    Added argparse CLI to main.py (#4)
    * started creating CLI using argparse on main.py
    
    * added dependency to gitignore
    
    * Delete backend/gitscribe.egg-info directory
    
    * Delete backend/build/lib/gitscribe directory
    
    * deleted build folder
    
    * added type argument to argparse

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,19 @@
 import argparse
+parser = argparse.ArgumentParser(prog="gitscribe")
+parser.add_argument("--type", type= str, choices=['modify', 'add', 'remove'], help="what type of commit, either: modify, add, or remove")
+parser.add_argument("changed_filename", help="filename of changed file", type=str)
+parser.add_argument("old_filename", help="filename old file", type=str, nargs='?', default='no_file_given')
 
+args = parser.parse_args()
+commit_type = args.type
+
+if (commit_type == "modify" and args.old_filename != "no_file_given"):
+    print("Your changed file is " + args.changed_filename + "\nYour old file is: " + args.old_filename)
+   # getchange(args.filename)
+elif (commit_type == "add"):
+    print("added file: " + args.changed_filename)
+elif (commit_type == "remove"):
+    print("removed file: " + args.changed_filename)
+
+else:
+    print("Input given not valid.")

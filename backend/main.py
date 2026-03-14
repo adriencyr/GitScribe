b6828ef0d56445bd13390d972920b4bb4ad588fc
Adrien Cyr <adrienjcyr@gmail.com>
2026-03-14 16:18:51 -0700

    feat(api): change structure of backend api response

--- backend/main.py
+++ backend/main.py
@@ -6,4 +6,6 @@ app = FastAPI()
 
 @app.get("/items")
 async def get_items(nm: int, old_filename: str, new_filename: str):
-    return generate_commit_message_from_file(nm, old_filename, new_filename)
+    return {
+        "messages": generate_commit_message_from_file(nm, old_filename, new_filename)
+    }

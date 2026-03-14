5992c31dfad0671c2cff9fbc81a209598d9e519f
shiang723 <hanhanshiang6@gmail.com>
2026-03-14 15:53:31 -0700

    Adding a post request point to api main.py file

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-from gitscribe.generator import generate_file_commit_message
+#from gitscribe.generator import generate_file_commit_message
+from pydantic import BaseModel
 
+class Upload(BaseModel):
+    id: str
+    num_msgs: int
+    old_filename: str
+    new_filename: str
+    
 app = FastAPI()
 
 
-@app.get("/items")
-async def get_items(nm: int, old_filename: str, new_filename: str):
-    return generate_file_commit_message(nm, old_filename, new_filename)
+@app.post("/upload_items/{item_id}")
+async def create_upload_item(upload_item: Upload):
+    return(upload_item)
+    #return generate_file_commit_message(nm, old_filename, new_filename)
+
+@app.get("/upload_items_get/")
+async def upload_items(num_msgs: int, old_filename: str, new_filename: str):
+    return ("{" + num_msgs + " " + old_filename + new_filename + "}")

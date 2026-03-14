442b08d58c40f533c17158e97982bb03c3592915
Adrien Cyr <adrienjcyr@gmail.com>
2026-03-13 20:27:17 -0700

    chore: initial package setup config

--- backend/setup.py
+++ backend/setup.py
@@ -1,2 +1,19 @@
 from setuptools import setup
 
+setup(
+    name='gitscribe',
+    version='0.1.0',
+    packages=['gitscribe'],
+    entry_points={
+        'console_scripts': ['gitscribe=gitscribe.main:main']
+    },
+    install_requires=[
+        'argparse',
+        'openai',
+        'python-dotenv',
+        'setuptools'
+    ],
+    description='A tool to generate commit messages using OpenAI models.',
+    author='Author',
+    python_requires='>=3.12'
+)

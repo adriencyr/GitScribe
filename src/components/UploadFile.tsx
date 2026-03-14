764754c2731d4b3b1ce7e00ade85cf43224ceaef
CelestiaWana <wyang73@my.bcit.ca>
2026-03-14 16:47:07 -0700

    Feature/upload buttons (#17)
    * button update
    
    * update buttons
    
    * Upload Submission Button and upload File button
    
    * add button for upload
    
    * upload button
    
    * upload seletion of text and upload file, generater box

--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -1,17 +1,41 @@
-import { Button, FileUpload } from "@chakra-ui/react";
+"use client";
+
+import { Button, FileUpload, Stack } from "@chakra-ui/react";
 import { HiUpload } from "react-icons/hi";
 
 const UploadFile = () => {
   return (
-    <FileUpload.Root directory>
-      <FileUpload.HiddenInput />
-      <FileUpload.Trigger asChild>
-        <Button variant="outline" size="sm">
-          <HiUpload /> Upload file
-        </Button>
-      </FileUpload.Trigger>
-      <FileUpload.List />
-    </FileUpload.Root>
+    <Stack gap="4" align="stretch">
+      <FileUpload.Root
+        accept={[
+          ".java",
+          ".py",
+          ".c",
+          ".cpp",
+          ".js",
+          ".ts",
+          ".jsx",
+          ".tsx",
+          ".html",
+          ".css",
+        ]}
+      >
+        <FileUpload.HiddenInput />
+
+        <FileUpload.Trigger asChild>
+          <Button variant="outline" size="sm" width="fit-content">
+            <HiUpload /> Upload file
+          </Button>
+        </FileUpload.Trigger>
+
+        <FileUpload.List />
+      </FileUpload.Root>
+
+      <Button colorPalette="teal" type="submit" alignSelf="flex-end">
+        Submit
+      </Button>
+    </Stack>
   );
 };
+
 export default UploadFile;

6f2caa7e33bf8b910f5ea7a03cc106cddd2263af
shiang723 <123040434+shiang723@users.noreply.github.com>
2026-03-14 20:01:49 -0700

    updated post request to retrieve front end request (#22)
    * started creating CLI using argparse on main.py
    
    * added dependency to gitignore
    
    * Delete backend/gitscribe.egg-info directory
    
    * Delete backend/build/lib/gitscribe directory
    
    * deleted build folder
    
    * added type argument to argparse
    
    * added number of messages argument to gitscribe CLI on main.py
    
    * added get query on main.py under api folder
    
    * Adding a post request point to api main.py file
    
    * added redis local data cache to fastapi endpoints
    
    * update the path in main.py
    
    * updated main.py for fastapi to connect with frontend post request

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,48 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Form, UploadFile, File
 #from gitscribe.generator import generate_commit_message_from_file
 import redis
+
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.redis_db import pool
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_redis():
       return redis.Redis(connection_pool=pool)
 
 
 
-@app.get("/uploads/{item_id}")
+@app.get("/uploads/")
 async def read_msgs(item_id: str, cache = Depends(get_redis)):
      messages = cache.get(item_id)
      return {"msgs": messages}
 
-@app.post("/uploads/{item_id}")
-async def update_item(item_id: str, num_msgs: int, old_filename: str, new_filename: str, cache = Depends(get_redis)):
-  cache.set(item_id, old_filename)
-  return {"uploaded": old_filename }
+@app.post("/uploads/")
+async def update_item(
+    user_id: str = Form(...), 
+    num_msgs: int = Form(...), 
+    old_file: UploadFile = File(...), 
+    new_file: UploadFile = File(...), 
+    cache = Depends(get_redis)):
+  old_file_content = await old_file.read()
+  new_file_content = await new_file.read()
+  generated_msgs = "changed file to be xyz"
+  cache.set(user_id, generated_msgs)
+  return {"user_files_uploaded":  user_id}
 
 @app.post("/texts/{item_id}")
 async def create_item(item_id:str, num_msgs: int, old_filename: str, new_filename: str, cache = Depends(get_redis)):

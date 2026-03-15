da1ad9e7258c837bbe63fa5459d4fb0fb9c846df
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-14 22:19:26 -0700

    fix: fix bugs

--- src/components/NumberInputComponent.tsx
+++ src/components/NumberInputComponent.tsx
@@ -1,16 +1,19 @@
-import { NumberInput } from "@chakra-ui/react"
+import {Field ,NumberInput } from "@chakra-ui/react"
 
 type NumberInputComponentProp = {
-  handleNumberInputChangeEvent: (value:string) => void;
+  handleNumberInputChangeEvent: (value: string) => void;
   numMsgs: string;
 }
 
-const NumberInputComponent = ({handleNumberInputChangeEvent, numMsgs}:NumberInputComponentProp) => {
+const NumberInputComponent = ({ handleNumberInputChangeEvent, numMsgs }: NumberInputComponentProp) => {
   return (
-    <NumberInput.Root defaultValue="10" width="200px" value={numMsgs} onValueChange={(details) => {handleNumberInputChangeEvent(details.value)}}>
-      <NumberInput.Control />
-      <NumberInput.Input />
-    </NumberInput.Root>
+    <Field.Root>
+      <Field.Label>Choose the number of commit messages</Field.Label>
+      <NumberInput.Root defaultValue="10" width="200px" value={numMsgs} onValueChange={(details) => { handleNumberInputChangeEvent(details.value) }}>
+        <NumberInput.Control />
+        <NumberInput.Input />
+      </NumberInput.Root>
+    </Field.Root>
   )
 }
 
--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import NumberInputComponent from "./NumberInputComponent";
 
-const UploadForm = () => {
+const UploadForm = ({handleUpdateMessages}:{handleUpdateMessages:(value:string)=>void}) => {
     console.log(`${import.meta.env.VITE_AUTH0_URL}/uploads`);
     const [oldFile, setOldFile] = useState<File | null>(null);
     const [newFile, setNewFile] = useState<File | null>(null);
@@ -27,18 +27,18 @@ const UploadForm = () => {
         }
     }
 
-    function handleNumberInputChangeEvent(value:string){
+    function handleNumberInputChangeEvent(value: string) {
         setNumMsgs(value);
     }
 
-    async function uploadFiles(old_file:File, new_file:File, num_msgs:string, user_id:string) {
+    async function uploadFiles(old_file: File, new_file: File, num_msgs: string, user_id: string) {
         try {
             const formData = new FormData();
             formData.append("old_file", old_file)
             formData.append("new_file", new_file)
             formData.append("num_msgs", num_msgs)
             formData.append("user_id", user_id)
-            
+
             const response = await axios.post(`${import.meta.env.VITE_AUTH0_URL}/uploads`, formData);
             return response;
         } catch (error) {
@@ -46,19 +46,20 @@ const UploadForm = () => {
         }
     }
 
-    async function handleSubmitForm(e:React.MouseEvent<HTMLButtonElement>) {
-        try{
-        e.preventDefault();
-        if (oldFile && newFile && user?.sub) {
-            //make first api request to upload file
-            const response1 = await uploadFiles(oldFile, newFile, numMsgs, user.sub)
-            
-            if(response1?.data){
-                const response2 = await axios.get(`${import.meta.env.VITE_AUTH0_URL}/msgs/${user.sub}`)
-                console.log(response2?.data)
+    async function handleSubmitForm(e: React.MouseEvent<HTMLButtonElement>) {
+        try {
+            e.preventDefault();
+            if (oldFile && newFile && user?.sub) {
+                //make first api request to upload file
+                const response1 = await uploadFiles(oldFile, newFile, numMsgs, user.sub)
+
+                if (response1?.data) {
+                    const response2 = await axios.get(`${import.meta.env.VITE_AUTH0_URL}/msgs/${user.sub}`)
+                    console.log(response2?.data)
+                }
             }
-        }}
-        catch(e){
+        }
+        catch (e) {
             console.log("Submit error" + e)
         }
     }
@@ -67,8 +68,8 @@ const UploadForm = () => {
         <div>
             <UploadFile fileName="oldFile" handleFileInputChangeEvent={handleFileInputChangeEvent} />
             <UploadFile fileName="newFile" handleFileInputChangeEvent={handleFileInputChangeEvent} />
-            <NumberInputComponent numMsgs = {numMsgs} handleNumberInputChangeEvent={handleNumberInputChangeEvent}/>
-            <Button colorPalette="teal" type="submit" alignSelf="flex-end" onClick={(e:React.MouseEvent<HTMLButtonElement>)=>{handleSubmitForm(e)}}>
+            <NumberInputComponent numMsgs={numMsgs} handleNumberInputChangeEvent={handleNumberInputChangeEvent} />
+            <Button colorPalette="teal" type="submit" alignSelf="flex-end" onClick={(e: React.MouseEvent<HTMLButtonElement>) => { handleSubmitForm(e) }}>
                 Submit
             </Button>
         </div>

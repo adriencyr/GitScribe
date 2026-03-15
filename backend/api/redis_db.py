28ad6080fc5972e0303ce23c33a28b14018313b0
shiang723 <hanhanshiang6@gmail.com>
2026-03-14 20:49:35 -0700

    started implementing redis cloud database

--- backend/api/redis_db.py
+++ backend/api/redis_db.py
@@ -1,15 +1,20 @@
 # Source - https://stackoverflow.com/a/73585930
 # Posted by Tegar
 # Retrieved 2026-03-14, License - CC BY-SA 4.0
-
+import os
+from dotenv import load_dotenv
 import redis
+
+load_dotenv()
 
 def create_redis():
   return redis.ConnectionPool(
-    host='localhost', 
-    port=6379, 
-    db=0, 
-    decode_responses=True
+    #max_connections=20,
+    host= os.getenv("REDIS_HOST"),
+    port=19281, 
+    decode_responses=True,
+    username=os.getenv("REDIS_USER"),
+    password=os.getenv("REDIS_PASSWORD"),
   )
 
 pool = create_redis()
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from api.redis_db import pool
 
-app = FastAPI()
+def get_redis():
+      return redis.Redis(connection_pool=pool)
+
+async def lifespan(app: FastAPI):
+    """Initializes and closes the Redis connection during app lifespan."""
+    global redis_client
+    redis_client = get_redis()
+    yield
+    await redis_client.close()
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost",
@@ -21,14 +31,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def get_redis():
-      return redis.Redis(connection_pool=pool)
 
 
 
-@app.get("/uploads/")
-async def read_msgs(item_id: str, cache = Depends(get_redis)):
-     messages = cache.get(item_id)
+
+@app.get("/msgs/")
+async def read_msgs(user_id: str, cache = Depends(get_redis)):
+     messages = cache.get(user_id)
      return {"msgs": messages}
 
 @app.post("/uploads/")
@@ -42,12 +51,16 @@ async def update_item(
   new_file_content = await new_file.read()
   generated_msgs = "changed file to be xyz"
   cache.set(user_id, generated_msgs)
-  return {"user_files_uploaded":  user_id}
+  return {"user_id":  user_id,
+          "old_file_type": old_file.content_type,
+          "old_file_content": old_file_content,
+          "new_file_type": new_file.content_type,
+          "new_file_content": new_file_content}
 
-@app.post("/texts/{item_id}")
-async def create_item(item_id:str, num_msgs: int, old_filename: str, new_filename: str, cache = Depends(get_redis)):
-    cache.set(item_id, num_msgs, old_filename, new_filename)
-    return {"item_id": item_id}
+# @app.post("/texts/{item_id}")
+# async def create_item(item_id:str, num_msgs: int, old_filename: str, new_filename: str, cache = Depends(get_redis)):
+#     cache.set(item_id, num_msgs, old_filename, new_filename)
+#     return {"item_id": item_id}
 
 
 #     return {
